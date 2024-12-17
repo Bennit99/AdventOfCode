3304635e637ece8a60fa29e6f8bbcd595d1a79c6
bstrack <bstrack@uni-muenster.de>
2024-12-17 20:43:03 +0100

    parsing result

--- 2024/17b.py
+++ 2024/17b.py
@@ -1,7 +1,11 @@
 from aocd import data, submit
 import re
-import time
-from concurrent.futures import ProcessPoolExecutor, as_completed
+
+result = 0
+data = list(map(int, re.findall(r"\d+", data)))
+program = data[3:]
+B = data[1]
+C = data[2]
 
 def combo(operand, A, B, C):
     if operand == 4:
@@ -12,11 +16,17 @@ def combo(operand, A, B, C):
         return C
     return operand
 
-def process_chunk(start_counter, end_counter, B, C, program):
-    for A_counter in range(start_counter, end_counter):
+solutions = [set() for _ in range(len(program))]
+
+def check(s, i, digits, C=C, B=B, program=program):
+    for counter in range(0, 2**digits):
+        A = counter*(2**len(s)) + int(s, 2)
+        if A < 2**43:
+            A += 2**43
+        A_innit = A
         output = []
         instruction_pointer = 0
-        A = A_counter
+        relevant_digits = 3 
         while instruction_pointer < len(program):
             opcode = program[instruction_pointer]
             operand = program[instruction_pointer + 1]
@@ -39,46 +49,34 @@ def process_chunk(start_counter, end_counter, B, C, program):
                 output.append(combo(operand, A, B, C) % 8)
                 if output != program[:len(output)]:
                     break
+                else:
+                    if len(output) == i+1:
+                        x = bin(A)
+                        y = str(x)
+                        z = y[-relevant_digits:]
+                        possible = str(bin(A_innit))[-relevant_digits:]
+                        if possible not in solutions[i]:
+                            print(i+1,': ', possible)
+                            solutions[i].add(possible)
             elif opcode == 6:
                 B = A // (2 ** combo(operand, A, B, C))
             elif opcode == 7:
                 C = A // (2 ** combo(operand, A, B, C))
+                relevant_digits = B+(3*(i+1))
             instruction_pointer += 2
 
         if output == program:
-            return A_counter
-    return None
+            result = A_innit
+            print(result)
+            submit(result)
+            break
 
-if __name__ == '__main__':
-    result = 0
-    data = list(map(int, re.findall(r"\d+", data)))
-    program = data[3:]
-    B = data[1]
-    C = data[2]
 
-    start = time.time()
-    record = 0
-    chunk_size = 10000000
-    num_workers = 1
-
-    start_counter = 1000000000000000000000000000000000000000000
-
-    with ProcessPoolExecutor(max_workers=num_workers) as executor:
-        futures = [executor.submit(process_chunk, i, i + chunk_size, B, C, program) for i in range(start_counter, start_counter + chunk_size * num_workers, chunk_size)]
-        start_counter += chunk_size * (num_workers-1)
-
-        found = False
-        while not found:
-            for future in as_completed(futures):
-                result = future.result()
-                if result is not None:
-                    print(result)
-                    # submit(result)
-                    found = True
-                    break
-                # Submit a new task to keep the search going
-                start_counter += chunk_size
-                print(f"A_counter: {start_counter} Time elapsed: {(time.time() - start)/60:.2f} minutes")
-                futures.append(executor.submit(process_chunk, start_counter, start_counter + chunk_size, B, C, program))
-            
+for i in range(0, len(program)):
+    if i == 0: # innit
+        check('0', i, 10)
+    for s in solutions[i-1]:
+        digits = 10 + i*3 - len(s)
+        check(s, i, digits)
+    print('Solutions: ', len(solutions[i]))
 
